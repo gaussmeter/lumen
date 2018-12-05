db084929de1ec3209e2e262911bcf6334477f21c
Leland Sindt <leland@sway.org>
2018-12-05 13:49:14 +0000

    parse JSON

--- lumen.py
+++ lumen.py
@@ -3,19 +3,57 @@ import time
 import threading
 import logging
 import queue
+import json
+import board
+import neopixel
 
-hostName = "*"
 hostPort = 9000
 
-def lumen(queue):
+lumenCommand = { 'animation' : 'None' } 
+pixel_pin = board.D18
+num_pixels = 12
+ORDER = neopixel.GRBW
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,pixel_order=ORDER)
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos*3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos*3)
+        g = 0
+        b = int(pos*3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos*3)
+        b = int(255 - pos*3)
+    return (r, g, b) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
+
+def lumen(queue, event):
+  global pixel_index
+  global pixel
   logging.debug('start lumen')
   while True:
     while not queue.empty():
-      message = queue.get()
-      logging.debug(message)
-      if message == "stop":
-        logging.debug('stop lumen')
-        return
+      command = queue.get()
+      logging.debug(command)
+    if event.isSet():
+      logging.debug('stop lumen')
+      return
+    time.sleep(.01)
+    if command['animation'] == "rainbow":
+      for j in range(255):
+        for i in range(num_pixels):
+          pixel_index = (i * 255 // num_pixels) + j
+          pixels[i] = wheel(pixel_index & 255)
+        pixels.show()
 
 logging.basicConfig(
   level=logging.DEBUG,
@@ -24,37 +62,65 @@ logging.basicConfig(
 
 class MyServer(BaseHTTPRequestHandler):
   def do_GET(self):
-    self.send_response(200)
-    self.send_header("Content-type", "text/html")
-    self.end_headers()
-    self.wfile.write(bytes("ok %s" % self.path, "utf-8"))
-    if self.path == "/rainbow":
-      logging.debug('rainbow')
-      lumenQueue.put("rainbow")
-    if self.path == "/silon":
-      logging.debug('silon')
-      lumenQueue.put("silon")
-    if self.path == "/off":
-      logging.debug('off') 
-      lumenQueue.put("off")
+    global lastLumenPUT
+    if self.path == "/lumen": 
+      self.send_response(200)
+      self.send_header("Content-type", "json")
+      self.end_headers()
+      self.wfile.write(bytes(json.dumps(lumenCommand), "utf-8"))
+    else:
+      self.send_response(302)
+      self.send_header("Location", "/lumen")
+      self.end_headers()
+
+  def do_PUT(self):
+    global lumenCommand 
+    if self.path == "/lumen":
+      length = self.headers['Content-Length']
+      lumenCommand = parseCommand(self.rfile.read(int(length)).decode("utf-8")) 
+      if lumenCommand != None:
+        self.send_response(200)
+        self.send_header("Content-type", "json")
+        self.end_headers()
+        self.wfile.write(bytes('{ "response" : "ok" }', "utf-8"))
+        lumenQueue.put(lumenCommand)
+      else:
+        self.send_response(400)
+        self.send_header("Content-type", "json")
+        self.end_headers()
+        self.wfile.write(bytes('{ "response" : "fail"}', "utf-8"))
+    else:
+      self.send_response(404)
+      self.end_headers()
+
+def parseCommand(payload):
+  try:
+    return json.loads(payload)
+  except:
+    return None
 
 myServer = HTTPServer(('', hostPort), MyServer)
-print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
+print(time.asctime(), "Server Starts - *:%s" % (hostPort))
 
 lumenQueue = queue.Queue()
+lumenQueue.put(lumenCommand)
+lumenQuit = threading.Event()
 
 lumenThread = threading.Thread(
   name = 'lumen',
   target = lumen,
-  args = (lumenQueue,),
+  args = (lumenQueue,lumenQuit,),
 )
 lumenThread.start()
+
+
 
 try:
   myServer.serve_forever()
 except KeyboardInterrupt:
   pass
 
-lumenQueue.put("stop")
+lumenQuit.set()
 myServer.server_close()
-print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
+print(time.asctime(), "Server Stops - *:%s" % (hostPort))
+
